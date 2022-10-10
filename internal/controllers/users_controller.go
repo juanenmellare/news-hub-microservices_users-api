3b1a7ac2a94afeab72b3865ff76a0827790343ed
juanenmellare <juanmellare@gmail.com>
2022-10-10 17:16:30 -0300

    temp

--- internal/controllers/users_controller.go
+++ internal/controllers/users_controller.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"news-hub-microservices_users-api/internal/errors"
@@ -26,9 +27,13 @@ func (u usersController) Create(context *gin.Context) {
 
 	uuid := u.userService.Create(*request.FirstName, *request.LastName, *request.Email, *request.Password)
 
+	fmt.Println("5")
+
 	response := rest.NewCreateUserResponse(uuid)
+	fmt.Println("6")
 
 	context.JSON(http.StatusCreated, response)
+	context.Done()
 }
 
 func (u usersController) Authenticate(context *gin.Context) {
@@ -36,12 +41,17 @@ func (u usersController) Authenticate(context *gin.Context) {
 	request.MarshallAndValidate(context)
 
 	user := u.userService.Authenticate(*request.Email, *request.Password)
+	fmt.Println("5")
 
 	token := rest.NewUserToken(8, user)
+	fmt.Println("6")
 
 	response := rest.NewAuthenticateResponse(token.ToString(u.userTokenSecretKey))
 
+	fmt.Println("7")
+
 	context.JSON(http.StatusOK, &response)
+	context.Done()
 }
 
 func (u usersController) Get(context *gin.Context) {
@@ -57,6 +67,7 @@ func (u usersController) Get(context *gin.Context) {
 	response := rest.NewGetResponse(user)
 
 	context.JSON(http.StatusOK, &response)
+	context.Done()
 }
 
 func NewUsersController(userService services.UsersService, userTokenSecretKey string, userTokenExpirationHours int) UsersController {
--- internal/services/users_service.go
+++ internal/services/users_service.go
@@ -26,22 +26,33 @@ func (u usersService) Create(firstName, lastName, email, password string) uuid.U
 		panic(errors.NewAlreadyExistModelError(fmt.Sprintf("user with '%s' email", email)))
 	}
 
+	fmt.Println("1")
+
 	user := models.NewUser(firstName, lastName, email, password, u.bCryptCost)
+
+	fmt.Println("2")
+
 	u.usersRepository.Create(user)
+
+	fmt.Println("3")
 
 	return user.ID
 }
 
 func (u usersService) Authenticate(email, password string) *models.User {
+	fmt.Println("1")
 	userFounded := u.usersRepository.FindByEmail(email)
 	if userFounded == nil {
 		panic(errors.NewInvalidEmailOrPasswordError())
 	}
+	fmt.Println("2")
 
 	doesPasswordMatch := utils.VerifyPassword(password, userFounded.Password)
+	fmt.Println("3")
 	if !doesPasswordMatch {
 		panic(errors.NewInvalidEmailOrPasswordError())
 	}
+	fmt.Println("4")
 
 	return userFounded
 }
